0621b936d43122b6ae400a86815ba044ca97990e
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-15 23:02:50 -0500

    Added comments

--- spinner2.js
+++ spinner2.js
@@ -1,9 +1,11 @@
 const characters = ['|', '/', '-', '\\', '|', '/', '-', '\\'];
 const timeouts = [100, 300, 500, 700, 900, 1100, 1300, 1500];
 
+//Iterate through the characters array 
 for (let i = 0; i < characters.length; i++) {
   setTimeout(() => {
+    //Every iteratation, print a different character into the same line
     process.stdout.write(`\r${characters[i]}   `);
-  }, timeouts[i]);
+  }, timeouts[i]); //Every iteration adds 1 to i which will change its index position (higher timeout)
 }
 
